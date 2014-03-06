35ebd5d75d946741438fe4da53611c704b10d890
Jonathan Peppers <jonathan.peppers@gmail.com>
2014-03-05 21:38:59 -0600

    Added test project

---
+++ Ninject.Tests.iOS/Ninject.Tests.iOS/Tests/NinjectTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Ninject;
+using NUnit.Framework;
+using Ninject.Modules;
+
+namespace Ninject.Tests.iOS
+{
+    [TestFixture]
+    public class NinjectTests
+    {
+        class Clazz : InYourFace
+        {
+        }
+
+        interface InYourFace
+        {
+        }
+
+        class Module : NinjectModule
+        {
+            public override void Load()
+            {
+                this.Bind<InYourFace>().To<Clazz>();
+            }
+        }
+
+        [Test]
+        public void StandardImport()
+        {
+            var kernel = new StandardKernel(new Module());
+
+            var inYourFace = kernel.Get<InYourFace>();
+
+            Assert.IsNotNull(inYourFace);
+        }
+    }
+}
+
